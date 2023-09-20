90a693217f7329cbaa895d4c9f282ad863d4fa50
Kyle Huang <kylehuang06@gmail.com>
2023-09-20 10:41:21 -0700

    Changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import process_data
+import random as rand
 
 df = process_data.filter_planet_and_date()
 
@@ -12,15 +13,27 @@ def get_main_char(df: pd.DataFrame) -> pd.DataFrame:
     potential_main = filter_appearances(df, 100, 300)
     potential_main = potential_main[potential_main["Alignment"] == "Good"]
     return potential_main
+
+def get_neutral_char(df: pd.DataFrame) -> pd.DataFrame:
+    potential_side = filter_appearances(df, 10, 100)
+    potential_side = potential_side[potential_side["Alignment"] == "Neutral"]
+    return potential_side
     
 def get_main_villain() -> pd.DataFrame:
     potential_villains = filter_appearances(df, 100, 300)
     potential_villains = potential_villains[potential_villains["Alignment"] == "Bad"]
     return potential_villains
 
+def get_side_villain(df: pd.Dataframe) -> pd.DataFrame:
+    potential_side_villain = filter_appearances(df, 1100, 300)
+    potential_side_villain = potential_side_villain[potential_side_villain["Alignment"] ]
+
 potential_main = get_main_char(df)
 potential_villains = get_main_villain(df)
 
-for index, row in potential_main.iterrows():
-    pass
+main_character = potential_main.sample()
+
+good_freq = 0
+evil_freq = 0
+
 
